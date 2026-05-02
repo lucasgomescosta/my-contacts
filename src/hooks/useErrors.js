f68ac0a6f549e42ed89f82d68b36b9eb1e5f65e1
lucas gomes <lucas.gomes@ibrowse-sds.com.br>
2026-05-02 15:57:24 -0400

    update

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 export default function useErrors() {
   const [errors, setErrors] = useState([]);
 
-  function setError({ field, message }) {
-    const errorAlreadyExists = errors.find((error) => error.field === field);
+  const setError = useCallback(({ field, message }) => {
+    const errorAlreadyExists =
+    errors.find((error) => error.field === field);
 
     if (errorAlreadyExists) {
       return;
     }
 
-    setErrors((prevState) => [...prevState, { field, message }]);
-  }
+    setErrors((prevState) => [
+      ...prevState,
+      { field, message }
+    ]);
+  }, [errors]);
 
-  function removeError(field) {
-    setErrors((prevState) => prevState.filter((error) => error.field !== field));
-  }
+  const removeError = useCallback((field) => {
+    setErrors((prevState) =>
+      prevState.filter((error) => error.field !== field)
+    );
+  }, [errors]);
 
-  function getErrorMessageFieldName(fieldName) {
-    return errors.find((error) => error.field === fieldName)?.message;
-  }
+  const getErrorMessageFieldName = useCallback((fieldName) => {
+    return errors.find(
+      (error) => error.field === fieldName
+    )?.message;
+  }, [errors]);
 
   return {
     errors,
